55d46763b2a0761f31049595679f6bfa9eafdea1
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2017-01-07 03:24:31 -0700

    create part by year

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -3,17 +3,21 @@ class Enrollment
 
   def initialize(hash)
     @data = hash
+    # binding.pry
   end
 
   def kindergarten_participation_by_year
     stored = {}
-    kinder_partiticipation_by_year = @data[:kindergarten_participation]
-    kinder_partiticipation_by_year.each { |year,data| stored[year] = data.to_s[0..4].to_f }
-    stored
+    kinder_by_year = @data[:kindergarten_participation]
+    kinder_by_year.each do |year,data|
+      stored[year] = data.to_s[0..4].to_f
+    end
+      stored
   end
-
-  def kindergarten_participation_by_year
-
-  end
+  #
+  # def kindergarten_participation_in_year(@data)
+  #   # binding.pry
+  #   @data
+  # end
 
 end
